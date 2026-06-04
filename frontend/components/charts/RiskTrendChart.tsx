dae3891cf03feccbb5aa1de236149b580ab7e339
MD.SHAHZAD HUSSAIN RAYIED <rayiedhussain8@gmail.com>
2026-06-05 03:02:30 +0600

    RiskTrendChart bug fixed

--- frontend/components/charts/RiskTrendChart.tsx
+++ frontend/components/charts/RiskTrendChart.tsx
@@ -2,12 +2,14 @@
 
 import {
     CartesianGrid,
+    Legend,
     Line,
     LineChart,
+    ReferenceArea,
     ResponsiveContainer,
     Tooltip,
     XAxis,
-    YAxis,
+    YAxis
 } from "recharts";
 
 type TrendPoint = {
@@ -24,9 +26,13 @@ export default function RiskTrendChart({
 }: Props) {
   const chartData = data.map(
     (item) => ({
-      date: new Date(
-        item.predicted_at
-      ).toLocaleDateString(),
+     date: new Date(item.predicted_at)
+  .toLocaleString([], {
+    month: "short",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  }),
       score: item.clinical_score,
     })
   );
@@ -35,24 +41,91 @@ export default function RiskTrendChart({
     <div className="w-full h-72">
       <ResponsiveContainer>
         <LineChart data={chartData}>
-          <CartesianGrid strokeDasharray="3 3" />
+          <CartesianGrid
+  stroke="#1e293b"
+  strokeDasharray="3 3"
+/>
 
           <XAxis dataKey="date" tick={{ fill: "#94a3b8", fontSize: 12 }} />
 
-          <YAxis
-            domain={[0, 100]}
-             tick={{ fill: "#94a3b8", fontSize: 12 }}
-          />
+       <YAxis
+  tick={{
+    fill: "#94a3b8",
+    fontSize: 12,
+  }}
+  label={{
+    value: "Clinical Score",
+    angle: -90,
+    position: "insideLeft",
+    fill: "#94a3b8",
+  }}
+  domain={[
+    (dataMin) => Math.max(0, dataMin - 2),
+  (dataMax) => dataMax + 2,
+  ]}
+/>
 
-          <Tooltip />
+       <Tooltip
+  contentStyle={{
+    background: "#131720",
+    border: "1px solid #1e2535",
+    borderRadius: "12px",
+    color: "#fff",
+  }}
+  formatter={(value) => {
+  const score = Number(value);
 
-          <Line
-            type="monotone"
-            dataKey="score"
-             stroke="#3b82f6"
-            strokeWidth={3}
-            dot={{ r: 4 }}
-          />
+  const risk =
+    score >= 6
+      ? "High"
+      : score >= 3
+      ? "Medium"
+      : "Low";
+
+  return [
+    `${score} (${risk})`,
+    "Clinical Score",
+  ];
+}}
+/>
+
+            <ReferenceArea
+  y1={0}
+  y2={3}
+  fill="#22c55e"
+  fillOpacity={0.08}
+/>
+
+<ReferenceArea
+  y1={3}
+  y2={6}
+  fill="#eab308"
+  fillOpacity={0.08}
+/>
+
+<ReferenceArea
+  y1={6}
+  y2={10}
+  fill="#ef4444"
+  fillOpacity={0.08}
+/>
+<Legend
+  wrapperStyle={{
+    color: "#94a3b8",
+    fontSize: "12px",
+  }}
+/>
+
+    <Line
+  type="stepAfter"
+  dataKey="score"
+  name="Clinical Score"
+  stroke="#3b82f6"
+  strokeWidth={3}
+  dot={{ r: 5 }}
+  activeDot={{ r: 8 }}
+  animationDuration={1200}
+/>
         </LineChart>
       </ResponsiveContainer>
     </div>
